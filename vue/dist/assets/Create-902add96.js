fb559cf7515208710f11115ed4c69a3063c4c89d
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-06-21 13:45:37 +0300

    new build

--- vue/dist/assets/Create-902add96.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./main-06caa501.js";const s={};function t(n,o){return c(),r("div",null," this is permissions create ")}const _=e(s,[["render",t]]);export{_ as default};
--- vue/dist/assets/Show-818548b3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c}from"./main-06caa501.js";const o={};function r(t,n){return s(),c("div",null," this is users show ")}const a=e(o,[["render",r]]);export{a as default};
