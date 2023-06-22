2409d5c610b827be739debf2185542e14528a5e7
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-06-22 08:41:30 +0300

    add new commmit

--- vue/dist/assets/Show-e75504a9.js
+++
@@ -1,1 +0,0 @@
-import{z as e,o as s,c as o}from"./main-c5724adf.js";const c={};function n(r,t){return s(),o("div",null," this is permissions show ")}const i=e(c,[["render",n]]);export{i as default};
