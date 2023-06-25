1ead9d6094e07b586c9b18b93eedbb27df7cb29d
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-06-25 08:19:44 +0300

    new build

---
+++ vue/dist/assets/Edit-59a2cd00.js
@@ -0,0 +1,1 @@
+import{z as e,o as s,c as t}from"./main-d4f867a4.js";const c={};function n(o,r){return s(),t("div",null," this is permissions edit ")}const a=e(c,[["render",n]]);export{a as default};
