49db346a7da7e6f674af8258e356545f9972ebb7
Mohamed <moahmedpro1@gmail.com>
2023-06-26 12:50:38 +0200

    print resulte

--- vue/src/views/children/ChildResult.vue
+++ vue/src/views/children/ChildResult.vue
@@ -4,10 +4,9 @@ import Chart from 'chart.js/auto';
 import moment from "moment";
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
+
 
 export default {
-  components:{InputText},
 
   data() {
     return {
@@ -55,7 +54,7 @@ export default {
         this.growAge = []
         this.diffAge = []
         this.result.forEach((elem) => {
-          this.created_at.push(moment(elem.result_created_at).format("DD-MM-YYYY"))
+          this.created_at.push(moment(elem.result_created_at).format("MM-DD-YYYY"))
           this.latePercenteges.push(elem.late_percentage)
           this.growAge.push(elem.grow_age)
           this.diffAge.push(elem.diff_age)
@@ -124,7 +123,7 @@ export default {
       })
     },
     formateDate(date) {
-      return moment(date).format('YYY-MM-DD HH:mm')
+      return moment(date).format('DD-MM-YYYY HH:mm')
     },
     print() {
       //     document.getElementById('print').style.display="block";
@@ -159,7 +158,7 @@ export default {
         this.growAge = []
         this.diffAge = []
         this.result.forEach((elem) => {
-          this.created_at.push(moment(elem.result_created_at).format("YYY-MM-DD"))
+          this.created_at.push(moment(elem.result_created_at).format("DD-MM-YYYY"))
           this.latePercenteges.push(elem.late_percentage)
           this.growAge.push(elem.grow_age)
           this.diffAge.push(elem.diff_age)
@@ -337,11 +336,11 @@ export default {
         {title: 'id'},
 
         {key: 'therapist_name', title: this.$t('therapist_name')},
+        {key: 'child_age', title: this.$t('child_age')},
         {key: 'grow_age', title: this.$t('grow_age')},
         {key: 'diff_age', title: this.$t('diff_age')},
-        {key: 'late_percentage', title: this.$t('late_percentage')},
         {key: 'basal_age', title: this.$t('basal_age')},
-        {key: 'child_age', title: this.$t('child_age')},
+        {key: 'late_percentage', title: this.$t('late_percentage')},
         {key: 'result_created_at', title: this.$t('created_at')},
         {title: this.$t('operation')}
 
@@ -489,11 +488,11 @@ export default {
           <tr>
             <td>{{ index + 1 }}</td>
             <td>{{ item.columns.therapist_name }}</td>
+            <td>{{ item.columns.child_age }} months</td>
             <td>{{ item.columns.grow_age }}</td>
             <td>{{ item.columns.diff_age }}</td>
-            <td>{{ Math.round(item.columns.late_percentage) }} %</td>
             <td>{{ item.columns.basal_age }} months</td>
-            <td>{{ item.columns.child_age }} months</td>
+            <td>{{ Math.round(item.columns.late_percentage) }} %</td>
             <td>{{ formateDate(item.columns.result_created_at) }}</td>
             <!--          <td>{{ moment(item.raw.result_created_at).format('DD-MM-YYYY') }}</td>-->
             <!--          <td>-->
@@ -504,9 +503,6 @@ export default {
 
               <Dialog v-model:visible="visible" modal header=" " :style="{ width: '50vw' }">
                 <v-form fast-fail @submit.prevent ref="form">
-                  <!-- <div style="width: 100%;" class="card flex  justify-content-center">
-                    <InputText style="width: 100%;" type="date" :rules="NameRules"  dateFormat="yy/mm/dd hh:mm" v-model="child.birth_date" />
-                </div> -->
                   <v-text-field
                       v-model="examDate"
                       :rules="NameRules"
@@ -534,7 +530,6 @@ export default {
         id="print"
         hide-default-footer
         disable-pagination
-
     >
 
       <template #top>
@@ -563,15 +558,11 @@ export default {
 
             <Dialog v-model:visible="visible" modal header=" " :style="{ width: '50vw' }">
               <v-form fast-fail @submit.prevent ref="form">
-                <div style="width: 100%;" class="card flex  justify-content-center">
-                  <InputText style="width: 100%;" type="date" :rules="NameRules"  dateFormat="yy/mm/dd hh:mm" v-model="child.birth_date" />
-              </div>
                 <v-text-field
                     v-model="examDate"
                     :rules="NameRules"
                     :label="$t('examDate')"
                     type="datetime-local"
-
                 ></v-text-field>
                 <button class="bg-blue pa-3 rounded" @click="submit">{{ $t('submit') }}</button>
               </v-form>
