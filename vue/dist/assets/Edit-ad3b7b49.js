079b708bccac34a587b92a54930d8179c8347e9e
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-07-25 15:25:40 +0300

    new build

--- vue/dist/assets/Edit-ad3b7b49.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t}from"./main-ddfe2247.js";const c={};function n(o,r){return s(),t("div",null," this is permissions edit ")}const _=e(c,[["render",n]]);export{_ as default};
---
+++ vue/dist/assets/overlayeventbus.esm-c95377ae.js
@@ -0,0 +1,1 @@
+import{p as r}from"./basecomponent.esm-d2481fdb.js";var e=r();export{e as O};
