495e6c6fd0db8778913e23141182f4d639a1afe4
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-08-20 14:44:22 +0300

    edit in migration file

--- laravel/database/migrations/2023_08_09_092712_add_image_column_parents_table.php
+++ laravel/database/migrations/2023_08_09_092712_add_image_column_parents_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('parents', function (Blueprint $table) {
-            $table->string("image")->change()->nullable();
+            $table->string("image")->nullable();
         });
     }
 
