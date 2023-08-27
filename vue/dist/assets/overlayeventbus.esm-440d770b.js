b19813eb8a783eb57f81b808f19a95621c3b3b72
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-08-27 09:26:01 +0300

    solve an issue

---
+++ vue/dist/assets/overlayeventbus.esm-440d770b.js
@@ -0,0 +1,1 @@
+import{aM as r}from"./main-a67400b4.js";var e=r();export{e as O};
--- vue/src/main.js
+++ vue/src/main.js
@@ -31,8 +31,19 @@ import "animate.css";
 import "./axios";
 import "./style.css";
 import "./views/frontend/assets/main.css";
+
+let k= document.getElementsByClassName("switcher")
+console.log(k)
+if( i18n.global.locale.value = 'ar')
+k[0].dir="rtl"
+else
+  k[0].dir="ltr"
+
 import App from "@/App.vue";
 const pinia = createPinia();
+
+
+
 //
 //  main.js or main.ts
 
